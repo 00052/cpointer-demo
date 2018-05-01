3ba9e354c63b92be31158d8c91a8d05142d925f0
root <root@jmpesp.cc>
2018-05-01 09:58:26 -0400

    fix read func

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ static void _read(struct s1 *p) {
 	printf("p->ch : %c\n", p->ch);
 	printf("p->a->aa[0] : %d\n", *p->a->aa[0]);
 	printf("p->a->ab[0] : %d\n", *p->a->ab[0]);
-	printf("p->a->ab[1] : %d\n", *p->a->ab[1]);
+	printf("p->a->ab[1] : %d\n", (*p->a->ab)[1]);
 
 	return ;
 }
