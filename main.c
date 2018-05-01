4437aad01c2d80bdca7aabd5081ef4173f2cd7c8
poilynx <lihsilyn@gmail.com>
2018-05-01 21:07:31 +0800

    Add Makefile

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 
 #pragma pack(push,1)
-	struct s1 {
-		struct {int *aa; int *ab;} * a; 
-		char ch;
-	};
+struct s1 {
+	struct {
+		int (*aa)[1]; 
+		int (*ab)[2];
+	} * a; 
+	char ch;
+};
 #pragma pack(pop)
 
 static void * _alloc(void) {
@@ -27,13 +30,13 @@ static void _fill(void *p) {
 
 static void _read(struct s1 *p) {
 	printf("p->ch : %c\n", p->ch);
-	printf("p->a->aa[0] : %d\n", p->a->aa[0]);
-	printf("p->a->ab[0] : %d\n", p->a->ab[0]);
-	printf("p->a->ab[1] : %d\n", p->a->ab[1]);
+	printf("p->a->aa[0] : %d\n", *p->a->aa[0]);
+	printf("p->a->ab[0] : %d\n", *p->a->ab[0]);
+	printf("p->a->ab[1] : %d\n", *p->a->ab[1]);
 
 	return ;
 }
-int main() {
+int main(void) {
 	struct s1* p;
 
 	p = _alloc();
